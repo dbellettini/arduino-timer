2c9b8abc8e1eaf92abf9dbc2f40733dbc30bbf60
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2024-06-30 12:59:11 +0200

    Add missing begin

--- boot.py
+++ boot.py
@@ -7,4 +7,6 @@ b.pulse(.1)
 
 d = Display(Pin(18, Pin.OUT), Pin(17, Pin.OUT),
             [Pin(10, Pin.OUT), Pin(9, Pin.OUT), Pin(8, Pin.OUT), Pin(5, Pin.OUT)])
+
+d.begin(16, 2)
 d.test_sentence()
